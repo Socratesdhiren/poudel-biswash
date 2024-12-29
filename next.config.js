2a5951e720522493ebd5844d31f8b6545c73d070
Dhirendra <dhirendra.arje@grepsr.com>
2024-12-29 11:34:18 +0545

    resolve the export issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  // output: "export", // Ensures a static export
+  // output: "standalone",
+  output: "export", // Ensures a static export
   basePath: "/poudel-biswash", // Use your repository name
   images: {
     unoptimized: true, // Disable image optimization for GitHub Pages
