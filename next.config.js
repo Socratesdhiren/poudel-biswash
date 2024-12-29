d821f854f14313c0f69d3ee65cbe582b84c421ab
Dhirendra <dhirendra.arje@grepsr.com>
2024-12-29 11:09:11 +0545

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/poudel-biswash",
   images: {
+    unoptimized: true,
     domains: ["localhost"],
     remotePatterns: [
       {
