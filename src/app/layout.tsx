39c37bccf944a3f2b97706c215080e77cc1c4038
Dhirendra <dhirendra.arje@grepsr.com>
2025-01-14 09:00:03 +0545

    remove clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,17 +42,16 @@ export default function RootLayout({
           <Footer />
           <ScrollToTop />
         </Providers>
-        {process.env.NEXT_PUBLIC_ENV === "production" && (
-          <Script id="clarity-script" strategy="afterInteractive">
-            {`
+
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
             (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "${CLARITY_ID}");
           `}
-          </Script>
-        )}
+        </Script>
       </body>
     </html>
   );
